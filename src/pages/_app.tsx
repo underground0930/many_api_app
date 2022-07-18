ccb0ae6512cba9967a874653a961eba38b6d2d11
underground0930 <go.nishiduka.1985@gmail.com>
2022-07-18 19:57:23 +0900

    update: rootに RecoilRootを追加

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,16 @@
-
 import type { AppProps } from 'next/app'
-
-import {
-  QueryClient,
-  QueryClientProvider,
-} from 'react-query'
+import { RecoilRoot } from 'recoil'
+import { QueryClient, QueryClientProvider } from 'react-query'
 
 const queryClient = new QueryClient()
 
-
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <QueryClientProvider client={queryClient}>
-      <Component {...pageProps} />
-    </QueryClientProvider>
+    <RecoilRoot>
+      <QueryClientProvider client={queryClient}>
+        <Component {...pageProps} />
+      </QueryClientProvider>
+    </RecoilRoot>
   )
 }
 
